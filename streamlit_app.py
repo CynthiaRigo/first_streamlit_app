a49970228f6f754fb258bdeac643188d5747c916
CynthiaRigo <73132695+CynthiaRigo@users.noreply.github.com>
2024-02-06 15:37:32 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
-
+ \
 import requests
 streamlit.header ("Fruityvice Fruit Advice!")
 
