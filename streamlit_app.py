5cd4ecb22f8b53358477bfe0c986aeaa0fa2658c
CynthiaRigo <73132695+CynthiaRigo@users.noreply.github.com>
2024-02-07 08:55:58 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,12 +42,6 @@ except URLError as e:
    streamlit.error()
 
 
-# streamlit.text(fruityvice_response.json())
-
-fruityvice_normalized = pandas.json_normalize (fruityvice_response.json())
-
-streamlit.dataframe(fruityvice_normalized)
-
 streamlit.stop()
 
 
